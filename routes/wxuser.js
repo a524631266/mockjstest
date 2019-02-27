5b36d80cc1862adce669ab1df4d4e78e4e15348f
zhangll <zhangll@homewell.com.cn>
2019-02-27 21:11:05 +0800

    更新微信好的接口

--- routes/wxuser.js
+++ routes/wxuser.js
@@ -3,7 +3,8 @@ var Mock = require("mockjs");
 var express = require('express');
 
 var router = express.Router();
-/* GET home page. */
+
+// 1.微信小程序登录并根据code返回 openid和 sessionid
 router.get('/wx/login/code/:code', function(req, res, next) {
   var token1 = token.createToken(req.params.code,10000);
   console.log("222222222222222login")
@@ -15,9 +16,20 @@ router.get('/wx/login/code/:code', function(req, res, next) {
   res.send(resa);
 //   res.render('index', { title: resa });
 });
+// 2.微信小程序获取歇后语
+router.get('/wx/everyday/day/:day', function(req, res, next) {
+    var data = Mock.mock(
+            {
+              imageurl:'@image()',
+              content:'@cparagraph(1)',
+            }
+          )
+    var resa = JSON.stringify(data);
+    res.send(resa);
+    //   res.render('index', { title: resa });
+});
 
-
-
+// 3.登录首页获取最新数量的赛事列表（日期降序排列）
 router.get('/wx/sportevents/size/:size', function(req, res, next) {
     var a = req.params.size
     var name  = 'arrs|'+ a;
@@ -40,6 +52,8 @@ router.get('/wx/sportevents/size/:size', function(req, res, next) {
     res.send(resa);
   //   res.render('index', { title: resa });
 });
+
+// 4.加载之前的数据(size代表数据限制数量 now 为传过去的时间发布时间，size为返回多少量默认最多10个
 router.get('/wx/sportevents/now/:now/size/:size', function(req, res, next) {
     var size = req.params.size;
     var start = req.params.now;
@@ -61,29 +75,7 @@ router.get('/wx/sportevents/now/:now/size/:size', function(req, res, next) {
     var resa = JSON.stringify(data.arrs);
     res.send(resa);
 });
-router.get('/wx/sportevents/last/:last/now/:now', function(req, res, next) {
-    var last = req.params.last;
-    var start = req.params.now;
-    var size = '@natural(0,5)';
-    var data = Mock.mock({
-        ['arrs|'+size]:[
-            {
-                'sid|1-60': 0,
-                "rid|1-43":1,
-                title:'@csentence(20)',
-                content:'@cparagraph(5)',
-                inserttime:'@natural(1531191176, 1541191176)',
-                starttime:'@natural(1531191176, 1541191176)',
-                Expiretime:'@natural(1531191176, 1541191176)',
-                imgurl:'@image()',
-                nums:'@natural(0,400)'
-            }
-        ]
-    });
-    var resa = JSON.stringify(data.arrs);
-    res.send(resa);
-    //   res.render('index', { title: resa });
-});
+// 5.该接口是用户刷新下拉更新最新时间的时候用的,根据last(10位时间戳) 和now(10位时间戳)获取数据
 router.get('/wx/sportevents/last/:last/now/:now', function(req, res, next) {
     var last = req.params.last;
     var start = req.params.now;
@@ -108,7 +100,10 @@ router.get('/wx/sportevents/last/:last/now/:now', function(req, res, next) {
     //   res.render('index', { title: resa });
 });
 
-router.get('/wx/sportevent/sid/:sid/state/:state', function(req, res, next) {
+// 6.赛事列表
+// a).当state为true的时候根据用户的openid返回报名状态用户可以根据openid
+// b).state为false的时候不用返回uidstate 用户可以根据openid
+router.get('/wx/sportevent/sid/:sid', function(req, res, next) {
     var sid = req.params.sid;
     var state = req.params.state;
     var size = '@natural(0,5)';
@@ -123,13 +118,13 @@ router.get('/wx/sportevent/sid/:sid/state/:state', function(req, res, next) {
             arrangement:'@cname( 40,100 )',
             rule:'@cname( 40,100 )',
             organizers:'@city( )',
-            uidstate: '@natural(2,4)',
+            uidstate: '@natural(2,4)', // 单个用户在当前下的报名条件
     });
     var resa = JSON.stringify(data.arrs);
     res.send(resa);
     //   res.render('index', { title: resa });
 });
-
+// 7.直接报名,不可重复报名切记
 router.post('/wx/entrytable/sid/:sid', function(req, res, next) {
     var sid = req.params.sid;
     var state = req.params.state;
@@ -138,7 +133,7 @@ router.post('/wx/entrytable/sid/:sid', function(req, res, next) {
     res.send(resa);
     //   res.render('index', { title: resa });
 });
-
+// 8.这里显示区域的列表，让用户筛选
 router.get('/wx/rid/all', function(req, res, next) {
     var sid = req.params.sid;
     var state = req.params.state;
@@ -153,14 +148,14 @@ router.get('/wx/rid/all', function(req, res, next) {
     res.send(resa);
     //   res.render('index', { title: resa });
 });
-
+// 8.手机验证，在后台用来注册用的
 router.get('/wx/sms/:phone', function(req, res, next) {
     var phone = req.params.phone;
     
     res.send(true);
     //   res.render('index', { title: resa });
 });
-
+// 8.1.用户注册信息 并返回uid 用户在数据库中的id编号,为了报名使用
 router.post('/wx/wxuser/register', function(req, res, next) {
     var name = req.query.name;
     var sex = req.query.sex;
@@ -178,10 +173,16 @@ router.post('/wx/wxuser/register', function(req, res, next) {
     console.log("rid",rid)
     console.log("agree",agree)
     console.log("comm",comm)
-    res.send(true);
+    var data = Mock.mock({
+        "arr|1-10":[{
+            "uid|0-5":1,
+        }]}
+    );
+    var resa = JSON.stringify(data.arr);
+    res.send(resa);
     //   res.render('index', { title: resa });
 });
-
+// 9.用户点击个人页，获取所有相关信息，没有的电话返回空
 router.get('/wx/wxuser/info', function(req, res, next) {
     var data = Mock.mock(
             {
@@ -210,16 +211,6 @@ router.get('/wx/wxuser/info', function(req, res, next) {
     //   res.render('index', { title: resa });
 });
 
-router.get('/wx/everyday/day/:day', function(req, res, next) {
-    var data = Mock.mock(
-            {
-              imageurl:'@image()',
-              content:'@cparagraph(1)',
-            }
-          )
-    var resa = JSON.stringify(data);
-    res.send(resa);
-    //   res.render('index', { title: resa });
-});
+
 
 module.exports = router;
