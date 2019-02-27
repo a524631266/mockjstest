3fe9ac2ac8ae87d3b30cfa4d6666e93aba98b07b
zhangll <zhangll@homewell.com.cn>
2019-02-27 20:52:29 +0800

    添加后端系统接口

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,28 @@
-const Mock = require("mockjs");
+var Mock = require("mockjs");
 var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const data = Mock.mock({
+  var data = Mock.mock({
     "list|0-10":[
       {
         'id|+1':1
       }
     ]
   });
-  const resa = JSON.stringify(data);
+  var resa = JSON.stringify(data);
   res.render('index', { title: resa });
 });
 
 router.get('/mockjs', function(req, res, next) {
-  const data = Mock.mock({
+  var data = Mock.mock({
     "list|0-10":[
       {
         'id|+1':1
       }
     ]
   });
-  const resa = JSON.stringify(data);
+  var resa = JSON.stringify(data);
   // res.render('index', { title: resa });
   res.send(resa);
 });
